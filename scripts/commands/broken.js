faa8392e539cf8d96b58c88cdd3e194c5c086e5b
Masumahamed555 <ahamedmdmasum03@gmail.com>
2024-08-23 22:00:26 +0600

    Update broken.js

--- scripts/commands/broken.js
+++ scripts/commands/broken.js
@@ -21,7 +21,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["🌺🥰-আলভী-চৌধুরী-💚🌻"];
+   var hi = ["🌺🥰-মাসুম-আহমেদ💚🌻"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
