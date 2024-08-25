831bbb92ead7e5eb609f24934417c88d775cb790
HM MASUM <ahamedmdmasum03@gmail.com>
2024-08-25 21:42:04 +0600

    Update 1admin.js

--- scripts/commands/1admin.js
+++ scripts/commands/1admin.js
@@ -21,7 +21,7 @@ const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
 var link =["https://i.postimg.cc/fTV8nkJh/20230726-020132.jpg", 
             
-            "https://i.postimg.cc/cJzJzDjW/Screenshot-20230730-182443.jpg", 
+            "https://i.postimg.cc/1RVNrq7Z/Messenger-creation-15dbf147-078e-4693-8bf0-56b7df6f9f40.jpg", 
             
 "https://i.postimg.cc/N0yxZjDz/Screenshot-20230730-191933.jpg",
             
